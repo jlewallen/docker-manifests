0027ac1ab9db755af4774d73a34aeb39f19727b1
Jacob Lewallen <jlewallen@gmail.com>
2013-08-14 08:16:13 -0700

    No more vagrant stuff.

--- vagrant/ansible-bootstrap.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-apt-get update
-apt-get -y install ansible
-cp -f /vagrant/vagrant/ansible_hosts /etc/ansible/hosts
-chmod 644 /etc/ansible/hosts
-ansible-playbook -v --connection=local /vagrant/vagrant/basic.yml
